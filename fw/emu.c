4292e5ebb0272c7718c097008fad5096972c3237
Matlo <mat.lau@laposte.net>
2015-11-23 20:37:21 +0100

    Further improvements.

--- fw/emu.c
+++ fw/emu.c
@@ -14,29 +14,39 @@
 #define USART_DOUBLE_SPEED false
 
 /*
- * This is a block of memory to store all descriptors.
+ * The access to these variables is synchronized.
  */
+static uint8_t control[MAX_CONTROL_TRANSFER_SIZE];
+
+static s_epData input;
+static uint8_t inputDataLen;
+
 static uint8_t descriptors[MAX_DESCRIPTORS_SIZE];
 static s_descIndex descIndex[MAX_DESCRIPTORS];
 static s_endpoint endpoints[MAX_ENDPOINTS];
 
+/*
+ * Only used in the serial interrupt.
+ */
 static uint8_t * pdesc = descriptors;
-
 #if MAX_DESCRIPTORS > 255 / 5
 static uint8_t * pindex = descIndex;
 #endif
 
-static s_epData input; // 66 bytes (should not exceed 255 bytes)
-static uint8_t inputDataSize;
+/*
+ * Only used in the main.
+ */
+static uint8_t outEndpoints[MAX_ENDPOINTS];
+static uint8_t selectedOutEndpoint = 0;
+static uint8_t outEndpointNumber = 0;
 
 /*
- * These variables are used in both the main and serial interrupt,
+ * These variables are used in both the main and the serial interrupt,
  * therefore they have to be declared as volatile.
  */
 static volatile uint8_t started = 0;
 static volatile uint8_t controlReply = 0;
 static volatile uint8_t controlReplyLen = 0;
-static volatile int8_t selectedOutEndpoint = -1;
 
 static inline void forceHardReset(void) {
 
@@ -61,8 +71,6 @@ static inline void send_control_header(void) {
     Serial_SendByte(len);
     Serial_SendData(&USB_ControlRequest, sizeof(USB_ControlRequest));
 }
-
-static uint8_t buf[MAX_CONTROL_TRANSFER_SIZE];
 
 #define READ_VALUE_INC(TARGET) \
     while (value_len--) { \
@@ -112,11 +120,11 @@ ISR(USART1_RX_vect) {
     return;
     l_control:
     controlReplyLen = value_len;
-    READ_VALUE(buf)
+    READ_VALUE(control)
     controlReply = 1;
     return;
     l_in:
-    inputDataSize = value_len - 1;
+    inputDataLen = value_len - 1;
     READ_VALUE((uint8_t*)&input)
     return;
 }
@@ -163,10 +171,8 @@ void EVENT_USB_Device_ConfigurationChanged(void) {
             Endpoint_ConfigureEndpoint(endpoints[i].number, endpoints[i].type, endpoints[i].size, 1);
         }
         //TODO MLA: other endpoint types
-        if(selectedOutEndpoint == -1) {
-            if((endpoints[i].number & ENDPOINT_DIR_MASK) == ENDPOINT_DIR_OUT) {
-                selectedOutEndpoint = i;
-            }
+        if((endpoints[i].number & ENDPOINT_DIR_MASK) == ENDPOINT_DIR_OUT) {
+            outEndpoints[outEndpointNumber++] = i;
         }
     }
 }
@@ -200,7 +206,7 @@ void EVENT_USB_Device_UnhandledControlRequest(void) {
         send_control_header();
         while(!controlReply);
         Endpoint_ClearSETUP();
-        Endpoint_Write_Control_Stream_LE(buf, controlReplyLen);
+        Endpoint_Write_Control_Stream_LE(control, controlReplyLen);
         Endpoint_ClearOUT();
     } else {
         static uint8_t buffer[MAX_CONTROL_TRANSFER_SIZE];
@@ -220,7 +226,7 @@ void SendNextInput(void) {
 
         if (Endpoint_IsINReady()) {
 
-            Endpoint_Write_Stream_LE(input.data, inputDataSize, NULL);
+            Endpoint_Write_Stream_LE(input.data, inputDataLen, NULL);
 
             Endpoint_ClearIN();
 
@@ -231,27 +237,9 @@ void SendNextInput(void) {
     }
 }
 
-static void setNextOutEndpoint(void) {
-    if(selectedOutEndpoint != -1) {
-        int8_t i;
-        for (i = selectedOutEndpoint + 1; i < sizeof(endpoints) / sizeof(*endpoints) && endpoints[i].number; ++i) {
-            if ((endpoints[i].number & ENDPOINT_DIR_MASK) == ENDPOINT_DIR_OUT) {
-                selectedOutEndpoint = i;
-                return;
-            }
-        }
-        for (i = 0; i < selectedOutEndpoint && endpoints[i].number; ++i) {
-            if ((endpoints[i].number & ENDPOINT_DIR_MASK) == ENDPOINT_DIR_OUT) {
-                selectedOutEndpoint = i;
-                return;
-            }
-        }
-    }
-}
-
 void ReceiveNextOutput(void) {
 
-    if (selectedOutEndpoint >= 0) {
+    if (outEndpointNumber > 0) {
 
         static struct {
             struct {
@@ -261,10 +249,12 @@ void ReceiveNextOutput(void) {
             s_epData value;
         } packet = { .header.type = E_TYPE_OUT };
 
-        s_endpoint * endpoint = endpoints + selectedOutEndpoint;
-        packet.value.endpoint = endpoint->number;
+        s_endpoint * endpoint = endpoints + outEndpoints[selectedOutEndpoint++];
+        if (selectedOutEndpoint == outEndpointNumber) {
+            selectedOutEndpoint = 0;
+        }
 
-        setNextOutEndpoint();
+        packet.value.endpoint = endpoint->number;
 
         Endpoint_SelectEndpoint(endpoint->number);
 

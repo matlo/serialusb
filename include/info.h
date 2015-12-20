c46a65ef936a90d53d330ae2675db2da43f63fd5
Matlo <mat.lau@laposte.net>
2015-12-20 18:53:28 +0100

    Add basic arguments.

---
+++ include/info.h
@@ -0,0 +1,22 @@
+#ifndef INFO_H_
+#define INFO_H_
+
+#define INFO_VERSION "0.00"
+#define INFO_YEAR "2010-2015"
+#define INFO_DEV "Matlo"
+#define INFO_WEB "http://gimx.fr"
+#define INFO_LICENCE "GNU GPL v3"
+
+#ifdef __x86_64__
+  #define INFO_ARCH "x86_64"
+#endif
+
+#ifdef __i386__
+  #define INFO_ARCH "i386"
+#endif
+
+#ifdef __ARM_ARCH_6__
+  #define INFO_ARCH "ARMv6"
+#endif
+
+#endif /* INFO_H_ */
--- sw/serialusb.c
+++ sw/serialusb.c
@@ -6,6 +6,65 @@
 #include <proxy.h>
 #include <signal.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <info.h>
+#include <getopt.h>
+
+static char * port = NULL;
+
+static void usage()
+{
+  printf("Usage: sudo serialusb --port /dev/ttyUSB0\n");
+}
+
+int args_read(int argc, char *argv[]) {
+
+  int ret = 0;
+  int c;
+
+  struct option long_options[] = {
+    /* These options don't set a flag. We distinguish them by their indices. */
+    { "version", no_argument, 0, 'v' },
+    { "port", required_argument, 0, 'p' },
+    { 0, 0, 0, 0 }
+  };
+
+  while (1) {
+    /* getopt_long stores the option index here. */
+    int option_index = 0;
+
+    c = getopt_long(argc, argv, "p:v", long_options, &option_index);
+
+    /* Detect the end of the options. */
+    if (c == -1)
+      break;
+
+    switch (c) {
+
+    case 'p':
+      port = optarg;
+      break;
+
+    case 'v':
+      printf("serialusb %s %s\n", INFO_VERSION, INFO_ARCH);
+      exit(0);
+      break;
+
+    case '?':
+      usage();
+      exit(-1);
+      break;
+
+    default:
+      printf("unrecognized option: %c\n", c);
+      ret = -1;
+      break;
+    }
+  }
+
+  return ret;
+}
 
 static void terminate(int sig) {
   proxy_stop();
@@ -16,13 +75,14 @@ int main(int argc, char * argv[]) {
   (void) signal(SIGINT, terminate);
   (void) signal(SIGTERM, terminate);
 
-  char * port = NULL;
+  int ret;
 
-  if (argc > 1) {
-    port = argv[1];
+  ret = args_read(argc, argv);
+  if(ret < 0) {
+    return -1;
   }
 
-  int ret = proxy_init();
+  ret = proxy_init();
 
   if (ret == 0 && port != NULL) {
     ret = proxy_start(port);
